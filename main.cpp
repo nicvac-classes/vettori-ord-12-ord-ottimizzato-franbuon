c018c13b83daf70076b7da054ab54326983e8e15
franbuon <francescogiuseppe.buono@marconibari.edu.it>
2025-10-01 18:52:36 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,42 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int N, i, j, t;
+    bool scambio;
+    cout<<"Quanti numeri vuoi inserire?"<<endl;
+    cin>>N;
+    int v[N];
+    i=0;
+    while(i<=N-1)
+    {
+        v[i]=random()%N*10-1;
+        i=i+1;
+    }
+    i=0;
+    scambio=true;
+    while(i<=N-1 & scambio)
+    {
+        scambio=false;
+        j=0;
+        while(j<=(N-2)-i)
+        {
+            if(v[j]>v[j+1])
+            {
+                t=v[j];
+                v[j]=v[j+1];
+                v[j+1]=t;
+                scambio=true;
+            }
+            j=j+1;
+        }
+        i=i+1;
+    }
+    i=0;
+    while(i<=N-1)
+    {
+        cout<<"Il "<<i+1<<"° numero è "<<v[i]<<endl;
+        i=i+1;
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
